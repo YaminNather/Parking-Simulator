b3360c548662dcfbf3a3fceb8a61210fb01dbba7
Yamin Nather <2001s.yn@gmail.com>
2021-02-23 03:17:49 +0530

    Basic Safety Check Listener Done.

--- Assets/Player/Safety Checker/Scripts/SafetyCheckListener.cs
+++ Assets/Player/Safety Checker/Scripts/SafetyCheckListener.cs
@@ -1,15 +1,77 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public interface ISafetyCheckListener
+public class SafetyCheckListener : MonoBehaviour
 {
-    void fOnEnterSafetyRange();
-    void fOnExitSafetyRange();
-    
-    void fOnEnterWarningRange();
-    void fOnExitWarningRange();
+    internal void fRecieveRayFromSafetyChecker(Ranges range)
+    {
+        mRecievedRay = true;
+        fSwitchRange(range);
+    }
 
-    void fOnEnterDangerRange();
-    void fOnExitDangerRange();
+    private void fSwitchRange(Ranges range)
+    {
+        if (range == mCurRange)
+            return;
+
+        switch (mCurRange)
+        {
+            case Ranges.Safety:
+                fOnExitSafetyRange();
+                break;
+
+            case Ranges.Warning:
+                fOnExitWarningRange();
+                break;
+
+            case Ranges.Danger:
+                fOnExitDangerRange();
+                break;
+        }
+
+
+        switch (range)
+        {
+            case Ranges.Safety:
+                fOnEnterSafetyRange();
+                break;
+
+            case Ranges.Warning:
+                fOnEnterWarningRange();
+                break;
+
+            case Ranges.Danger:
+                fOnEnterDangerRange();
+                break;
+        }
+
+        mCurRange = range;
+    }
+
+    protected virtual void LateUpdate()
+    {
+        if (mCurRange != Ranges.Safety && mRecievedRay == false)
+        {
+            fSwitchRange(Ranges.Safety);
+        }
+
+        mRecievedRay = false;
+    }
+
+    protected virtual void fOnEnterSafetyRange() {}
+
+    protected virtual void fOnExitSafetyRange() {}
+
+    protected virtual void fOnEnterWarningRange() {}
+
+    protected virtual void fOnExitWarningRange() {}
+
+    protected virtual void fOnEnterDangerRange() {}
+
+    protected virtual void fOnExitDangerRange() {}
+
+
+    private bool mRecievedRay = false;
+    internal Ranges mCurRange = Ranges.Safety;
 }
+
+public enum Ranges { Safety, Warning, Danger }
